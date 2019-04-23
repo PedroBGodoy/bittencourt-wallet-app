d9cb798fa1b4bec229b3777661c8a71126baf34c
PedroBGodoy <pedrohenrique.godoy@Outlook.com>
2019-04-23 13:23:54 +0000

    Test change from VM

--- mobile/src/styles/common.js
+++ mobile/src/styles/common.js
@@ -2,5 +2,5 @@ export const primaryColor = "#171B29";
 export const statusColor = "#141824";
 export const secondaryColor = "#C7581E";
 export const lighColor = "#F5F5F5";
-export const darkBlue = "#24293B";
+export const darkBlue = "#050505";
 export const errorColor = "#C52929";
